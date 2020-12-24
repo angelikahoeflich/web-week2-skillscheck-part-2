21b46171732f9912ebd8fbf2c0ddc7e44f4b945a
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-12-24 10:41:10 -0800

    array problems

--- js-competencies/arrays-2/arrays-2.js
+++ js-competencies/arrays-2/arrays-2.js
@@ -9,7 +9,13 @@
 //3rd element of the array with the string '42 ducks'. 
 //addMoreDucks should return the array.
 
-//Code here
+function addMoreDucks(arr){
+  for (let i = 0; i < arr.length; i++) {
+    if(arr.length > 4){
+      arr[2] === '42 ducks'
+    } else return arr[i];
+  }
+}
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,7 +24,9 @@
 //and if the array's length is greater than 4, removes 
 //the 3rd element of the array. cutItOut should return the array.
 
-//Code here
+function cutItOut(arr){
+  for
+}
 
 
 //////////////////PROBLEM 3////////////////////
